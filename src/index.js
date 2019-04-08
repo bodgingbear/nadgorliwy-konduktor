f427cf27643414dc35d3d258c7a6a59e0697aec7
Bartosz Legięć <bartosz@legiec.eu>
2019-04-08 09:25:35 +0200

    asd

--- src/index.js
+++ src/index.js
@@ -29,6 +29,7 @@ import p1 from "./assets/pixil-layer-P1.png";
 
 let currentLevel = 0;
 let flaga = true;
+let flaga2 = true;
 
 class Boot extends Phaser.Scene {
   constructor(config) {
@@ -146,21 +147,26 @@ class Boot extends Phaser.Scene {
 
     this.load.image("sumoAnim0", sumoAnim0Img);
     this.load.image("sumoAnim1", sumoAnim1Img);
-    this.load.audio('theme', [ 'https://legiec.io/NKUTDP/Kyoto Train Station Ambience 3D Sounds Japan (Sleep Rest Study Tokyo ASMR).mp3']);
-    this.load.audio('bij', [ 'https://legiec.io/NKUTDP/bij.mp3']);
-    this.load.audio('sumo', ['https://legiec.io/NKUTDP/Sumo.wav']);
-    this.load.audio('passenger', ['https://legiec.io/NKUTDP/1.wav']);
-    this.load.audio('gates', ['https://legiec.io/NKUTDP/2.wav']);
+    this.load.audio('theme', [ '/Kyoto Train Station Ambience 3D Sounds Japan (Sleep Rest Study Tokyo ASMR).mp3']);
+    this.load.audio('bij', [ '/bij.mp3']);
+    this.load.audio('sumo', ['/Sumo.wav']);
+    this.load.audio('passenger', ['/1.wav']);
+    this.load.audio('gates', ['/2.wav']);
   }
 
   create() {
     this.startTime = new Date();
 
-    this.music = this.sound.add('theme', {loop: true, volume: 0.5});
-    this.bij = this.sound.add('bij', {loop: true, volume: 0.3});
+    if (flaga2) {
+      this.music = this.sound.add('theme', {loop: true, volume: 0.5});
+      this.bij = this.sound.add('bij', {loop: true, volume: 0.15});
+  
+      this.music.play();
+      this.bij.play();
 
-    this.music.play();
-    this.bij.play();
+      flaga2 = false;
+    }
+
     this.sumoSound = this.sound.add('sumo')
     this.gatesSound = this.sound.add('gates')
 
